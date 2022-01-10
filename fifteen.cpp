ba86552b23768e3bfa93856b4bc0f30f7cabb6f2
rootvertex <rootvertex@gmail.com>
2022-01-11 03:42:28 +0700

    struct and board

--- fifteen.cpp
+++ fifteen.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
+struct stats {
+    int MATCHES; 
+    int MOVES;
+    long int TIME;
+    int MINMOVES;
+    int MINTIME;
+} stored;
+
+short int board[4][4];
+
 int main()                                 
 {
     return 0;
