a571a88c94789b299c58e61224c1610c9ea53a96
rootvertex <rootvertex@gmail.com>
2022-01-11 18:33:17 +0700

    add createboard func

--- fifteen.cpp
+++ fifteen.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void createboard();
+
 struct stats {
     int MATCHES; 
     int MOVES;
@@ -15,4 +17,27 @@ short int board[4][4];
 int main()                                 
 {
     return 0;
+}
+
+void createboard()                                      
+{
+    short int value=1,posI,posJ;                        
+    for(posI=0;posI<4;posI++)
+    {
+        for(posJ=0;posJ<4;posJ++)
+            board[posI][posJ]=0;                        
+    }
+    while(value<16)
+    {
+        posI=rand()%4;
+        posJ=rand()%4;                                  
+        if(posI*posJ!=9)                                
+        {
+            if(board[posI][posJ]==0)
+            {
+                board[posI][posJ]=value;                
+                value++;
+            }
+        }
+    }
 }
