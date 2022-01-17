a0f6ae4b9e73c8896482a5d8123e81f243773292
rootvertex <rootvertex@gmail.com>
2022-01-17 19:18:29 +0700

    Create functions.h

---
+++ src/functions.h
@@ -0,0 +1,52 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include<stdlib.h>
+#include<fstream>
+#include<iostream>
+#include<iomanip>
+#include<time.h>
+#include<windows.h>
+#include<stdbool.h>
+#include<conio.h>
+
+using namespace std;
+
+struct stats{           
+    int MATCHES;        
+    int MOVES;          
+    long int TIME;      
+    int MINMOVES;       
+    int MINTIME;        
+}stored;
+
+short int board[4][4];   
+
+clock_t START,CURR;     
+int moveCount=0;        
+bool firstHit=false;   
+bool readSuccess;       
+
+bool readStats();       
+BOOL cursorLocation(const WORD,const WORD);
+void menuoutline(const string&,const string&,const string&,const string&,const string&,const string&,const string&);
+void createboard();
+int checksolvable();
+char input(char);
+void displayboardFrame();
+int displayboard();
+void reset();
+void pausemenu();
+void stopwatch(int);
+void displayStopwatch();
+bool inputhandler(int);
+int checkboard();
+void gameOn();
+void updateStats();
+void statsView();
+void titlescreen();
+void swapfunc(short int*,short int*);
+void windowSetup();
+
+
+#endif
