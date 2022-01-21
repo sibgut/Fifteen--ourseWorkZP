6a96621822e9cab3f826ae9b2460e3abf5a9f770
rootvertex <rootvertex@gmail.com>
2022-01-22 00:37:51 +0700

    decompos

--- src/fifteen.cpp
+++ src/fifteen.cpp
@@ -29,86 +29,9 @@ bool readStats()
    return true;                            
 }
 
-void createboard()                                      
-{
-    short int value=1,posI,posJ;                        
-    for(posI=0;posI<4;posI++)
-    {
-        for(posJ=0;posJ<4;posJ++)
-            board[posI][posJ]=0;                        
-    }
-    while(value<16)
-    {
-        posI=rand()%4;
-        posJ=rand()%4;                                  
-        if(posI*posJ!=9)                                
-        {
-            if(board[posI][posJ]==0)
-            {
-                board[posI][posJ]=value;                
-                value++;
-            }
-        }
-    }
-}
-
-int checksolvable()                                             
-{
-    int invcount=0,GREATER,boardlist[16]={ 0 },num1,posI,posJ;
-    for(posI=0;posI<4;posI++)
-    {
-        for(posJ=0;posJ<4;posJ++)
-        {
-            if(board[posI][posJ])
-            {
-                GREATER=0;
-                for(num1=0;boardlist[num1];num1++)
-                {
-                    if(board[posI][posJ]>boardlist[num1]&&boardlist[num1])
-                        GREATER++;
-                }
-                invcount+=board[posI][posJ]-GREATER-1;
-                boardlist[num1]=board[posI][posJ];
-            }
-        }
-    }
-    return(invcount%2);                                         
-}
-
 char input(char Low)
 {
     return (Low>90)?Low-=32:Low;                                
-}
-
-
-int displayboard()                                             
-{
-    //system("cls");
-    system("clear");
-    short int zero, posI = 0, posJ = 0;                         
-    short int posY=3, posX = 30;                                
-    for( ; posY < 10 ; posY += 2, posI++){
-        cout << endl;
-        for( ; posX < 50 ; posX += 5, posJ++){
-            
-            if(board[posI][posJ]){
-                cout<<right<<setw(4)<<board[posI][posJ];
-            }
-            else{
-                cout<<"    ";
-                zero = (10 * posI) + posJ;
-            }
-            
-        }
-        posJ = 0;
-        posX = 30;
-        
-    }
-    cout << endl;
-    cout<<"moves "<<moveCount << endl;              
-    cout<<"esc\tpause" << endl;                           
-    cout<<"w a s d move"<< endl;
-    return zero;                                 
 }
 
 void displayStopwatch(){
@@ -204,25 +127,6 @@ bool inputhandler(int zero)
         stopwatch(-1);                  
     }
     return keyHit;
-}
-
-int checkboard()                        
-{
-    short int checkValue=1;
-    for(short int i=0;i<4;i++)
-    {
-        for(short int j=0;j<4;j++)
-        {
-          if(!(i==3&&j==3))
-          {
-              if(board[i][j]==checkValue)       
-                checkValue++;                  
-              else
-                return 0;                      
-          }
-        }
-    }
-    return 1;                                   
 }
 
 void gameOn()                           
