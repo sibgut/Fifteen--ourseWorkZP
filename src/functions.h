af804b7e06f80b2733e50869b2c87ac2ef536451
sibgut <94751924+sibgut@users.noreply.github.com>
2022-01-22 23:32:18 +0700

    add linux support

--- src/functions.h
+++ src/functions.h
@@ -1,46 +1,19 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-#include<stdlib.h>
-#include<fstream>
 #include<iostream>
-#include<iomanip>
-#include<time.h>
-#include<stdbool.h>
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <iomanip>
 using namespace std;
+#define szb 4
 
-struct stats{           
-    int MATCHES;        
-    int MOVES;          
-    long int TIME;      
-    int MINMOVES;       
-    int MINTIME;        
-}stored;
-
-short int board[4][4];   
-int _kbhit();
-int START = 0,CURR = 0;     
-int moveCount=0;        
-bool firstHit=false;   
-bool readSuccess;       
-bool readStats();       
-void createboard();
-int checksolvable();
-char input(char);
-int displayboard();
-void reset();
-void pausemenu();
-void stopwatch(int);
-void displayStopwatch();
-bool inputhandler(int);
-int checkboard();
-void gameOn();
-void updateStats();
-void statsView();
-void titlescreen();
-void swapfunc(short int*,short int*);
-
+void gameOn(int board[szb][szb]);
+void fillrandom(int board[szb][szb]);
+void displayboard(int board[szb][szb]);
+bool issolvable(int board[szb][szb]);
 
 #endif
